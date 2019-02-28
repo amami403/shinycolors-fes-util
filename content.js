a0c2b0a974521d965eecc6c900f04f5ef5bea9e1
강수성 <maxkss@gangsuseong-ui-iMac.local>
2019-03-01 02:20:16 +0900

    upload

--- content.js
+++ content.js
@@ -1,9 +1,16 @@
-var dealMeter = document.createElement('script');
-dealMeter.src = chrome.extension.getURL('dealMeter.js');
-dealMeter.onload = function() {
+var s = document.createElement('script');
+s.src = chrome.extension.getURL('/lib/jquery-3.3.1.min.js');
+s.onload = function () {
     this.remove();
+
+    var dealMeter = document.createElement('script');
+    dealMeter.src = chrome.extension.getURL('dealMeter.js');
+    dealMeter.onload = function () {
+        this.remove();
+    };
+    (document.head || document.documentElement).appendChild(dealMeter);
 };
-(document.head || document.documentElement).appendChild(dealMeter);
+(document.head || document.documentElement).appendChild(s);
 
 $(document).ready(function(){
     chrome.runtime.sendMessage({}, function(response) {
